9f34cb9cde96cfb0220c86184148bc195a6cde63
KOTE3 <sebastianmejiamatienko@gmail.com>
2026-03-25 16:42:02 -0500

    update

--- src/main/java/com/example/demo/repository/IDeliveryRepository.java
+++ src/main/java/com/example/demo/repository/IDeliveryRepository.java
@@ -10,3 +10,16 @@ public interface IDeliveryRepository extends JpaRepository<Delivery, Long> {
 
     List<Delivery> findByDroneCodeAndStatus(String droneCode, String status);
 }
+
+
+//Obtener los jugadores de una confederación específica, ordenados por fifa_score de maneradescendente.
+  //List<Player> findByCountryConfederationOrderByFifaScoreDesc(String confederation);
+
+  //Obtener el club donde actualmente juega un jugador dado un nombre, ignorando las mayúsculas
+  //List<Club> findByPlayerClubsPlayerNameIgnoreCaseAndPlayerClubsEndDateIsNull(String name);
+
+  //Obtener los clubes donde sus jugadores jueguen entre dos fechas específicas y un estadio específico
+//List<Club> findByPlayerClubsPlayerCountryHomeMatchesStadiumIgnoreCaseAndPlayerClubsPlayerCountryHomeMatchesMatchDateBetween(String stadium, String startDate, String endDate);
+
+ //5. Obtener los partidos en los que participan jugadores con posición de delantero pertenecientes a un club específico.
+//List<Match> findByHomeCountryPlayersPositionAndHomeCountryPlayersPlayerClubsClubName(String position, String clubName);
